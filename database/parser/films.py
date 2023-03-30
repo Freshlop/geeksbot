a5e4a106139499e667d5bda4601b46eb90cc2e93
freshlop <aitidolonbekov40@gmail.com>
2023-03-30 12:19:28 +0300

    commit

---
+++ database/parser/films.py
@@ -0,0 +1,50 @@
+from pprint import pprint
+
+import requests
+from bs4 import BeautifulSoup
+
+
+URL = "https://rezka.ag/films/"
+
+
+HEADERS = {
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36"
+}
+
+
+def get_html(url):
+    response = requests.get(url=url, headers=HEADERS)
+    return response
+
+
+def get_data_from_page(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    items = soup.find_all(
+        'div', class_="b-content__inline_item"
+    )
+    films = []
+    for item in items:
+        info = item.find('div', class_="b-content__inline_item-link").find('div').getText().split(", ")
+        film = {
+            "1. Фильм:": item.find('div', class_="b-content__inline_item-link").find('a').getText(),
+            "2. Ссылка:": item.find('div', class_="b-content__inline_item-link").find('a').get("href"),
+            "3. Год:": info[0],
+            "4. Страна:": info[1],
+            "5. Жанр:": info[2],
+        }
+        films.append(film)
+    return films
+
+
+def parser():
+    html = get_html(URL)
+    if html.status_code == 200:
+        films = []
+        for i in range(1, 2):
+            html = get_html(f"{URL}page/{i}/")
+            current_page = get_data_from_page(html.text)
+            films.extend(current_page)
+        return films
+    else:
+        raise Exception("Error in parser")
--- handlers/client.py
+++ handlers/client.py
@@ -2,10 +2,12 @@ from aiogram import types, Dispatcher
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from config import bot
 from keyboards.client_kb import start_markup
-
+from database.bot_db import sql_command_random
+from parser.films import parser
 
 # @dp.message_handler(commands=['start'])
 async def start_command(message: types.Message):
+
     await message.answer("Hello world!", reply_markup=start_markup)
 
 
@@ -53,3 +55,35 @@ def register_handlers_client(dp: Dispatcher):
     dp.register_message_handler(start_command, commands=['start'])
     dp.register_message_handler(help_command, commands=['help'])
     dp.register_message_handler(quiz_1, commands=['quiz'])
+
+async def get_films(message: types.Message):
+    films = parser()
+    count = int(message.text.split()[1])
+    for film in films:
+        count -= 1
+        if count == 0:
+            break
+        await message.answer(
+            # f"{film['2. Ссылка:']}\n\n"
+            f"<a href='{film['2. Ссылка:']}'>{film['1. Фильм:']}</a>\n"
+            f"<b>Переходи туда ↑</b>\n"
+            f"#Y{film['3. Год:']}\n"
+            f"#{film['4. Страна:']}\n"
+            f"#{film['5. Жанр:']}\n",
+            reply_markup=InlineKeyboardMarkup().add(
+                InlineKeyboardButton(
+                    "СМОТРЕТЬ",
+                    url=film['2. Ссылка:']
+                )
+            ),
+            parse_mode=ParseMode.HTML
+        )
+
+
+def register_handlers_client(dp: Dispatcher):
+    dp.register_message_handler(start_command, commands=['start'])
+    dp.register_message_handler(quiz_1, commands=['quiz'])
+    dp.register_message_handler(send_image, commands=['mem'])
+    dp.register_message_handler(help_command, commands=['help'])
+    dp.register_message_handler(get_random_user, commands=['get'])
+    dp.register_message_handler(get_films, commands=['films'])
